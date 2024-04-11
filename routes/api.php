5daead8f744fef23e4d0024058bb294aab22887a
rutger <rutger.pronk11@gmail.com>
2024-04-11 13:40:02 +0200

    Use route groups

--- routes/api.php
+++ routes/api.php
@@ -1,54 +1,50 @@
 <?php
 
-use App\Models\Dish;
-use Illuminate\Http\Request;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\DishController;
+use App\Http\Controllers\MenuController;
+use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// MENU RELATED ROUTES
+Route::prefix('menu')->group(function () {
+    Route::get('/', [MenuController::class, 'index']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::prefix('category')->group(function () {
+        Route::post('/{name}', [CategoryController::class, 'createCategory']);
+        Route::put('/{category}/{name}', [CategoryController::class, 'updateCategory']);
+        Route::delete('/{category}', [CategoryController::class, 'deleteCategory']);
+        Route::patch('/swap/{category}/{category}', [CategoryController::class, 'swapCategories']);
+    });
+
+    Route::post('/dish/{category}/{name}/{description}/{price}', [DishController::class, 'createDish']);
+    Route::put('/dish/{dish}/{name}/{description}/{price}', [DishController::class, 'updateDish']);
+    Route::delete('/dish/{dish}', [DishController::class, 'deleteDish']);
+    Route::patch('/dish/swap/{dish}/{dish}', [DishController::class, 'swapDishes']);
 });
 
-Route::get('/create/{name}/{description}/{price}', function ($name, $description, $price) {
-    return Dish::create([
-        'name' => $name,
-        'description' => $description,
-        'price' => $price,
-    ]);
+// MESSAGE RELATED ROUTES
+Route::prefix('contact')->group(function () {
+    Route::get('/', [ContactController::class, 'getMessages']);
+    Route::post('/', [ContactController::class, 'createMessage']);
+    Route::delete('/{contactMessage}', [ContactController::class, 'deleteMessage']);
 });
 
-Route::get('/update/{id}/{name}/{description}/{price}', function ($id, $name, $description, $price) {
-    $dish = Dish::findOrFail($id);
-    $dish->name = $name;
-    $dish->description = $description;
-    $dish->price = $price;
-    $dish->save();
-    return $dish;
+Route::prefix('reservation')->group(function () {
+    Route::get('/', [ReservationController::class, 'getReservations']);
+    Route::post('/', [ReservationController::class, 'createReservation']);
+    Route::patch('/', [ReservationController::class, 'acceptDeclineReservation']);
 });
 
-
-Route::get('/users', function () {
-    return User::all();
+// ADMIN RELATED ROUTES
+Route::prefix('admin')->group(function () {
+    Route::post('/{user}', [AdminController::class, 'promoteToAdmin']);
+    Route::delete('/{user}', [AdminController::class, 'demoteFromAdmin']);
 });
 
-
-Route::get('dish/{dish}/update', [DishController::class, 'update']);
-Route::get('/delete/{dish}', [DishController::class, 'delete']);
-
-Route::get('/user/create/{name}/{email}/{password}', function ($name, $email, $password) {
-    return User::create([
-        'name' => $name,
-        'email' => $email,
-        'password' => bcrypt($password),
-    ]);
+Route::prefix('auth')->group(function () {
+    Route::post('/{email}/{password}', [AuthController::class, 'loginAsAdmin']);
 });
