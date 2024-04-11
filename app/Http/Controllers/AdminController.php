da46164efb9ee7d7f28c74872ee0e99661d16a51
Rutger <rutger.pronk11@gmail.com>
2024-04-12 00:19:23 +0200

    Create logic for promoting and demoting users

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
 
 class AdminController extends Controller
 {
-    //
+    public function promoteToAdmin(User $user)
+    {
+        $user->update([
+            'is_admin' => true
+        ]);
+
+        return response()->json([
+            'message' => 'User has been promoted to admin'
+        ]);
+    }
+
+    public function demoteFromAdmin(User $user)
+    {
+        $user->update([
+            'is_admin' => false
+        ]);
+
+        return response()->json([
+            'message' => 'User has been demoted from admin'
+        ]);
+    }
 }
