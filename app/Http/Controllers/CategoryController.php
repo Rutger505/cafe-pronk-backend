9d1de803df52cc913fa9da186035aedcf73ff8bd
Rutger <rutger.pronk11@gmail.com>
2024-04-11 22:03:39 +0200

    Update category endpoint

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,15 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use Illuminate\Http\JsonResponse;
 
 class CategoryController extends Controller
 {
-    public function createCategory($name)
+    public function createCategory($name): JsonResponse
     {
         Category::create([
             'name' => $name
         ]);
 
         return response()->json(['message' => 'Category created successfully'], 201);
+    }
+
+    public function updateCategory(Category $category, $name): JsonResponse
+    {
+        $category->update([
+            'name' => $name
+        ]);
+
+        return response()->json(['message' => 'Category updated successfully']);
     }
 }
