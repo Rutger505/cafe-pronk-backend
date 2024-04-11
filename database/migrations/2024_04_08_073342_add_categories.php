3bf01fcf8f59de6c6979811d1e983bbdb32dd7b7
rutger <rutger.pronk11@gmail.com>
2024-04-11 12:29:32 +0200

    Fix migrations: remove autoIncrement because 2 ai (id and position_index) is not possible, rename date of add_categories file so that it runs before the dishes table

--- database/migrations/2024_04_08_073342_add_categories.php
+++ database/migrations/2024_04_08_073342_add_categories.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('position_index', true);
+            $table->integer('position_index');
         });
     }
 
