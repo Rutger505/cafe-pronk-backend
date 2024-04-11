63cf0493194b7d9642b8ce4999d325f7498dc80f
Rutger <rutger.pronk11@gmail.com>
2024-04-11 22:01:21 +0200

    Create category endpoint

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+    public function createCategory($name)
+    {
+        Category::create([
+            'name' => $name
+        ]);
+
+        return response()->json(['message' => 'Category created successfully'], 201);
+    }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,6 +12,20 @@ class Category extends Model
     public $timestamps = false;
     protected $table = 'categories';
 
+    protected $fillable = [
+        'name'
+    ];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($category) {
+            // Set the position_index to the next available value
+            $category->position_index = static::max('position_index') + 1;
+        });
+    }
+
     public function dishes()
     {
         return $this->hasMany(Dish::class);
