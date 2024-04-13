495fa8677f5252ad17d454cdce9afaa06953583f
Rutger <rutger.pronk11@gmail.com>
2024-04-14 00:40:46 +0200

    Create contact messages routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\DishController;
 use App\Http\Controllers\MenuController;
 use Illuminate\Support\Facades\Route;
@@ -18,17 +19,26 @@ Route::prefix('auth')->group(function () {
 Route::prefix('menu')->group(function () {
     Route::get('/', [MenuController::class, 'index']);
 
-    Route::group(['prefix' => 'category', 'middleware' => 'auth:sanctum'], function () {
+    Route::group(['prefix' => 'category', 'middleware' => ['auth:sanctum', 'adminOnly']], function () {
         Route::post('/', [CategoryController::class, 'store']);
         Route::put('/{category}/', [CategoryController::class, 'update']);
         Route::delete('/{category}', [CategoryController::class, 'delete']);
         Route::patch('/swap/{category1}/{category2}', [CategoryController::class, 'swap']);
     });
 
-    Route::group(['prefix' => 'dish', 'middleware' => 'auth:sanctum'], function () {
+    Route::group(['prefix' => 'dish', 'middleware' => ['auth:sanctum', 'adminOnly']], function () {
         Route::post('/', [DishController::class, 'store']);
         Route::put('/{dish}', [DishController::class, 'update']);
         Route::delete('/{dish}', [DishController::class, 'delete']);
         Route::patch('/swap/{dish1}/{dish2}', [DishController::class, 'swap']);
+    });
+});
+
+Route::prefix('contact')->group(function () {
+    Route::post('/', [ContactController::class, 'store']);
+
+    Route::middleware(['auth:sanctum', 'adminOnly'])->group(function () {
+        Route::get('/', [ContactController::class, 'index']);
+        Route::delete('/{contactMessage}', [ContactController::class, 'delete']);
     });
 });
