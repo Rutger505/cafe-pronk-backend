a5f38b2651d3e225dc62f37f0cf7648bee9021a0
Rutger <rutger.pronk11@gmail.com>
2024-04-14 22:57:37 +0200

    Update name to dish_order

--- database/migrations/2024_04_14_202639_create_dish_order.php
+++ database/migrations/2024_04_14_202639_create_dish_order.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('order_item_details', function (Blueprint $table) {
+        Schema::create('dish_order', function (Blueprint $table) {
             $table->id();
             $table->foreignId('order_id')->constrained()->onDelete('cascade');
             $table->foreignId('dish_id')->constrained();
